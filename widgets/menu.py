2114ec8e154a55dab7b6ea5231291aae6842ffa8
LoneHusko <90392521+LoneHusko@users.noreply.github.com>
2026-02-24 10:25:43 +0100

    menu button icons

--- widgets/menu.py
+++ widgets/menu.py
@@ -1,6 +1,10 @@
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QGuiApplication
+import os
+
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QGuiApplication, QColor
 from PySide6.QtWidgets import QFrame, QVBoxLayout, QPushButton, QSizePolicy, QAbstractScrollArea
+
+from modules.ui_helpers import svg_to_icon
 
 
 class Menu(QFrame):
@@ -32,19 +36,27 @@ class Menu(QFrame):
         }
         """)
 
+        icon_px = int(round(20 * self.scaleFactor))
+        btn_px = int(round(20 * self.scaleFactor))
+
+        button_color = QColor("#E6E6E6")
+
         self.stickers_button = QPushButton("Stickers")
-        self.stickers_button.setFixedSize(int(100 * self.scaleFactor), int(20 * self.scaleFactor))
+        self.stickers_button.setFixedSize(int(100 * self.scaleFactor), btn_px)
         self.stickers_button.clicked.connect(lambda checked=False, triggered="stickers":self.stacked_widget_trigger(triggered))
+        self.stickers_button.setIcon(svg_to_icon(os.path.join("utils", "ui", "sticker.svg"), QSize(icon_px, icon_px), button_color))
         self.layout.addWidget(self.stickers_button)
 
         self.notes_button = QPushButton("Notes")
-        self.notes_button.setFixedSize(int(100 * self.scaleFactor), int(20 * self.scaleFactor))
+        self.notes_button.setFixedSize(int(100 * self.scaleFactor), btn_px)
         self.notes_button.clicked.connect(lambda checked=False, triggered="notes":self.stacked_widget_trigger(triggered))
+        self.notes_button.setIcon(svg_to_icon(os.path.join("utils", "ui", "notebook.svg"), QSize(icon_px, icon_px), button_color))
         self.layout.addWidget(self.notes_button)
 
         self.bookmarks_button = QPushButton("Bookmarks")
-        self.bookmarks_button.setFixedSize(int(100 * self.scaleFactor), int(20 * self.scaleFactor))
+        self.bookmarks_button.setFixedSize(int(100 * self.scaleFactor), btn_px)
         self.bookmarks_button.clicked.connect(lambda checked=False, triggered="bookmarks":self.stacked_widget_trigger(triggered))
+        self.bookmarks_button.setIcon(svg_to_icon(os.path.join("utils", "ui", "bookmark.svg"), QSize(icon_px, icon_px), button_color))
         self.layout.addWidget(self.bookmarks_button)
 
         self.setFixedSize(self.layout.sizeHint())
